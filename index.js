aec60e1e77924a0fcebd95427f6c5f594861218d
Jim Park <jimp@scribd.com>
2022-01-27 16:37:54 -0800

    fix: if cannot parse err, just print

--- index.js
+++ index.js
@@ -683,9 +683,9 @@ async function run() {
 /* istanbul ignore next */
 if (require.main === module) {
   run().catch((err) => {
-    core.debug(`Received error: ${JSON.stringify(err)}`);
     const httpStatusCode = err.$metadata ? err.$metadata.httpStatusCode : undefined;
     core.setFailed(`${err.name} (Status code: ${httpStatusCode}): ${err.message}`);
+    core.debug(`Received error: ${JSON.stringify(err)}`).catch(() => { return err });
     core.debug(err.stack);
     process.exit(1);
   });
